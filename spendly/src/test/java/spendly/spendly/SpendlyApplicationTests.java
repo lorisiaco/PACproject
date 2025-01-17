ed0e99ae477b76145c8faca1b1871855919daee0
lorisiaco <loris.iacoban@gmail.com>
2025-01-17 14:44:46 +0100

    Creazione Spring

---
+++ spendly/src/test/java/spendly/spendly/SpendlyApplicationTests.java
@@ -0,0 +1,13 @@
+package spendly.spendly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
