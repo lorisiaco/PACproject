7347d521570be0207aaa664457c26a4d5774f339
lorisiaco <loris.iacoban@gmail.com>
2025-01-19 18:29:45 +0100

    -Collegato DB e creazione tabella utenti

--- spendly/src/main/java/spendly/spendly/UserController.java
+++ spendly/src/main/java/spendly/spendly/UserController.java
@@ -1,14 +1,34 @@
 package spendly.spendly;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@Controller
+
+@RestController
 public class UserController {
     
+    private final UserRepository userRepository;
+
+    UserController(UserRepository repository){
+        userRepository=repository;
+    }
+
     @GetMapping("/")
     public String homePage() {
         // Restituisce il file templates/index.html
         return "login";
     }
+
+    @GetMapping("/user")
+    Iterable<User> getUsers(){
+        return  userRepository.findAll();
+    }
+
+    @PostMapping("/user")
+    User creaUser(@RequestBody User newUser){   //Valore preso dal body della richiesta
+        return userRepository.save(newUser);
+    }
+    
 }
---
+++ spendly/src/main/java/spendly/spendly/UserRepository.java
@@ -0,0 +1,8 @@
+package spendly.spendly;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User,Long>{
+    
+    
+}
