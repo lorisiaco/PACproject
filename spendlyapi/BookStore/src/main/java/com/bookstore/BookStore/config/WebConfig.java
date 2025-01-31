3949bc00833287e9465f95473bd7c28d7c7845ab
lorisiaco <loris.iacoban@gmail.com>
2025-01-31 18:11:39 +0100

    -Sistemato Login-Register

---
+++ spendlyapi/BookStore/src/main/java/com/bookstore/BookStore/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.bookstore.BookStore.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173") // Cambia l'URL se necessario
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
