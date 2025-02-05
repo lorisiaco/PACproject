acad768eee3ad7a04918e4ed32422894dc5dc7ed
drossi15 <d.rossi15@studenti.unibg.it>
2025-02-05 14:46:05 +0100

    .

--- spendlyapi/BookStore/src/main/java/com/bookstore/BookStore/models/Group.java
+++ spendlyapi/BookStore/src/main/java/com/bookstore/BookStore/models/Group.java
@@ -2,6 +2,8 @@ package com.bookstore.BookStore.models;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,6 +11,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 
@@ -16,18 +19,13 @@ import jakarta.persistence.Table;
 @Table(name = "Gruppi")
 public class Group {
 
-     /**
-     * Id of the Group
-     */
+     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    /**
-     * Group Name
-     */
     private String nome;
 
+    @JsonIgnore
     @ManyToMany
     @JoinTable(
         name = "group_members",
@@ -35,67 +33,56 @@ public class Group {
         inverseJoinColumns = @JoinColumn(name = "user_id")
     )
 
-    /**
-     * Members of the Group
-     */
+    
     private List<AppUser> membri=new ArrayList<AppUser>();
 
+    @ManyToOne
+    @JoinColumn(name = "admin_id", nullable = false)
+    private AppUser admin;
 
-    /**
-     * Constructors
-     */
-    public Group(){}
+    //costruttori
+    public Group() {}
 
-    public Group(String n){
-        super();
-        this.nome=n;
+    public Group(String nome, AppUser admin) {
+        this.nome = nome;
+        this.admin = admin;
     }
+    public Group(String nome) {
+        this.nome = nome;
+        
+    }
+    
 
-    /**
-     * 
-     * @return return the Id of the Group
-     */
     public  Long getId(){
         return this.id;
     }
 
-    /**
-     * Set the Id ofr the Group
-     * @param id
-     */
     public void setId( Long id){
         this.id=id;
     }
 
-
-    /**
-     * 
-     * @return return the name of the Group
-     */
     public String getNome(){
         return this.nome;
     }
-    /**
-     * Set the nome for the group
-     * @param s
-     */
+    
     public void setNome(String s){
         this.nome=s;
     }
 
-    /**
-     * 
-     * @return return the Members of the Group
-     */
-    public List<AppUser> getMembri(){   // la lista non è immutabile come nome e id, dovrei stare attento a ritornare la lista vera
+    public List<AppUser> getMembri(){  
         return this.membri;
     }
 
+    public AppUser getAdmin(){
+        return this.admin;
+    }
 
-    /**
-     * Adding new member
-     * @param utente
-     */
+    public void setAdmin(AppUser Admin){
+        this.admin=Admin;
+    }
+
+
+    
     public void AggiungiMembro(AppUser utente){
         if(!membri.contains(utente)){
             membri.add(utente);
@@ -103,10 +90,7 @@ public class Group {
             throw new IllegalArgumentException("L'utente è già nel gruppo");
         }
     }
-    /**
-     * Removing member method
-     * @param utente
-     */
+    
 
     public void RimuoviMembro(AppUser utente){
         if(membri.contains(utente)){
@@ -115,12 +99,6 @@ public class Group {
             throw new IllegalArgumentException ("L'utente non è nel gruppo");
         }
     }
-
-    /**
-     * 
-     * @param utente
-     * @return a boolean to check if an user is a member of the group
-     */
 
     public boolean ContieneMembro(AppUser utente){
         return membri.contains(utente);
